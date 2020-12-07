a72c4458c75a8d3ced1a32af767914e57a3652ed
ejaz4 <73548455+ejaz4@users.noreply.github.com>
2020-12-07 23:00:26 +0000

    Create appjs1.js

---
+++ appcontent/appjs1.js
@@ -0,0 +1,5 @@
+try{
+document.getElementsByClassName("rounded-full transition ease-in-out duration-150 transform active:scale-95 font-medium inline-flex items-center justify-center focus:outline-none select-none text-sm border border-transparent disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 disabled:border-gray-200 dark-disabled:bg-gray-700 dark-disabled:text-gray-500 dark-disabled:border-gray-600 text-gray-600 bg-transparent dark:text-gray-300 hover:opacity-85 active:opacity-75 py-2.5 px-7")[0].setAttribute("style", "display: none;");
+} catch(err){
+
+}
